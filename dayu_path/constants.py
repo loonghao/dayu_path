8b82046628c5ba605e8879b69427f542bf5f1fb6
hao.long <hoolongvfx@gmail.com>
2019-05-18 14:30:39 +0800

    Clear up

--- dayu_path/constants.py
+++ dayu_path/constants.py
@@ -22,18 +22,20 @@ EXT_SINGLE_MEDIA = {'.mov': {},
                     '.r3d': {},
                     }
 
-EXT_SEQUENCE_MEDIA = {'.exr' : {},
-                      '.dpx' : {},
+EXT_SEQUENCE_MEDIA = {'.exr': {},
+                      '.dpx': {},
                       '.tiff': {},
-                      '.tif' : {},
-                      '.png' : {},
-                      '.jpg' : {},
-                      '.bmp' : {},
-                      '.dng' : {},
-                      '.ari' : {},
+                      '.tif': {},
+                      '.png': {},
+                      '.jpg': {},
+                      '.bmp': {},
+                      '.dng': {},
+                      '.ari': {},
                       }
 
-SCAN_IGNORE = {'start': ('.', '..', 'Thumb'),
-               'end'  : ('.tmp')}
+SCAN_IGNORE = {
+    'start': ('.', '..', 'Thumb'),
+    'end': ('.tmp')
+}
 
 NETWORK_FILE_SYSTEM = ('nfs', 'smbfs', 'remote', 'afp', 'ftp', 'snfs')
